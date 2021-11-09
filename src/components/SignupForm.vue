81872a41466f52e665da64021a66d6c644c107af
dragsec <draganyes@gmail.com>
2021-11-09 12:54:00 +0100

    front end1

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <form id="signup-form">
+        <div class="row">
+            <div class="col-12 form-group">
+                <label class="col-form-label col-form-label-lg">Nome <span class="text-danger">*</span></label>
+                <input type="text" v-model="nome" class="form-control form-control-lg">
+            </div>
+            <div class="col-12 form-group">
+                <label class="col-form-label col-form-label-lg">Cognome <span class="text-danger">*</span></label>
+                <input type="text" v-model="cognome" class="form-control form-control-lg">
+            </div>
+            <div class="col-12 form-group">
+                <label class="col-form-label col-form-label-lg">Email <span class="text-danger">*</span></label>
+                <input type="text" v-model="$v.email.$model" :class="{'is-invalid': validationStatus($v.email)}" class="form-control form-control-lg">
+                <div v-if="!$v.email.required.email" class="invalid-feedback">Inserire email corretta.</div>
+            </div>
+            <div class="col-12 form-group text-center padtop2">
+                <button class="btn btn-vue btn-lg col-4 ">Registra</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+import {required, email} from 'vuelidate/lib/validators'
+export default {
+    name: 'SignupForm',
+    data: function(){
+        return {
+            nome: '',
+            cognome: '',
+            email: ''
+        }
+    },
+    validations: {
+        email: {required, email}
+    },
+
+    mounted () {
+        console.log(this.nome)
+    },
+
+    methods: {
+
+
+        validationStatus: function(validation) {
+            return typeof validation != "undefined" ? validation.$error : false;
+        },
+
+        submit: function() {
+            this.$v.$touch();
+            if (this.$v.$pendding || this.$v.$error) return;
+        
+            alert('Data Submit')
+        }
+    }
+}
+</script>
+<style scoped>
+.btn-vue{
+    background-color: #42b983;
+    margin-top: 20px;
+    font-weight: bold;
+}
+
+</style>
